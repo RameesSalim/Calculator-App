b021205dfbacbb1bdb1bea97ef874da196e85e79
Ramees Salim <salim.ramees@gmail.com>
2017-10-30 14:03:03 +0530

    Added new for displaying the complete Result

--- app/src/main/java/com/flashtechnos/calculator/MainActivity.java
+++ app/src/main/java/com/flashtechnos/calculator/MainActivity.java
@@ -5,13 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button button0 , button1 , button2 , button3 , button4 , button5 , button6 ,
             button7 , button8 , button9 , buttonAdd , buttonSub , buttonDivision ,
             buttonMul , button10 , buttonC , buttonEqual ;
 
+
     EditText edt1 ;
+    TextView display;
     //Completed Java Code
 
     float mValueOne , mValueTwo ;
@@ -41,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         buttonC = (Button) findViewById(R.id.buttonC);
         buttonEqual = (Button) findViewById(R.id.buttoneql);
         edt1 = (EditText) findViewById(R.id.edt1);
+        display=(TextView) findViewById(R.id.display);
 
 
         button1.setOnClickListener(new View.OnClickListener() {
@@ -161,24 +165,24 @@ public class MainActivity extends AppCompatActivity {
 
                 if (mAddition == true){
 
-                    edt1.setText(mValueOne + mValueTwo +"");
                     mAddition=false;
+                    display.setText(mValueOne+"+"+mValueTwo+"="+(mValueOne + mValueTwo) +"");
                 }
 
 
                 if (mSubtract == true){
-                    edt1.setText(mValueOne - mValueTwo+"");
+                    display.setText(mValueOne+"-"+mValueTwo+"="+(mValueOne - mValueTwo) +"");
                     mSubtract=false;
                 }
 
                 if (mMultiplication == true){
-                    edt1.setText(mValueOne * mValueTwo+"");
+                    display.setText(mValueOne+"*"+mValueTwo+"="+(mValueOne * mValueTwo) +"");
                     mMultiplication=false;
                 }
 
-                if (mDivision == true){
-                    edt1.setText(mValueOne / mValueTwo+"");
-                    mDivision=false;
+                if (mDivision == true) {
+                    display.setText(mValueOne + "/" + mValueTwo + "=" + (mValueOne / mValueTwo) + "");
+                    mDivision = false;
                 }
             }
         });
@@ -187,6 +191,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 edt1.setText("");
+                display.setText("");
             }
         });
 
